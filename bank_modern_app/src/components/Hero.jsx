dc2bcc0b6f89ee1328ff510ef3d3fcee1ef6c55a
Abuh Solomon <abuhsolo05@gmail.com>
2022-08-16 13:06:54 +0100

    update

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -16,13 +16,6 @@ const Hero = () => {
          <span className='text-white'>1 Month</span>Account</p>
       </div>
       <div className='flex flex-row justify-between items-center w-full'>
-<<<<<<< HEAD
-        <h1 className='flex-1 font-poppins font-semibold sm:text-[72px] text-[52px] text-white'>
-          The Next <br  className='sm:block hidden'/> 
-          <span className='text-gradient'>Generation</span> { " " }
-          Payment Method
-        </h1>
-=======
           <h1 className='flex-1 font-poppins font-semibold sm:text-[72px] text-[52] text-white'>
             The Next <br className='sm:block hidden' /> {" "}
             <span className='text-gradient'>
